33e69cda806e0209fed64e0bf2a80c1ba1aa5907
Avnish Kumar <avnish@geninf.com>
2019-12-27 21:48:50 +0530

    Add Page

--- oClock/oClock.Shared/App.xaml.cs
+++ oClock/oClock.Shared/App.xaml.cs
@@ -68,7 +68,7 @@ namespace oClock
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(oClock.Shared.Views.Clock), e.Arguments);
                 }
                 // Ensure the current window is active
                 Windows.UI.Xaml.Window.Current.Activate();
