8da1ed5ca1d0040ceb7a0d6543a9f3421e34e844
Avnish Kumar <avnish@geninf.com>
2019-12-27 23:13:07 +0530

    Code

--- oClock/oClock.Shared/ViewModels/ClockViewModel.cs
+++ oClock/oClock.Shared/ViewModels/ClockViewModel.cs
@@ -6,13 +6,50 @@ namespace oClock.Shared.ViewModels
 {
     public class ClockViewModel : Observable
     {
-        readonly DispatcherTimer Timer;
+        readonly DispatcherTimer Timer = new DispatcherTimer();
         public ClockViewModel()
         {
-            Timer = new DispatcherTimer();
             Timer.Tick += Timer_Tick;
             Timer.Interval = new TimeSpan(0, 0, 1);
             Timer.Start();
+            CurrentCheckInTimeCommand = new RelayCommand(OnCurrentCheckInTimeCommandExecuted);
+            InputCheckInTimeCommand = new RelayCommand(OnInputCheckInTimeCommandExecuted);
+        }
+
+        private void OnInputCheckInTimeCommandExecuted()
+        {
+
+        }
+
+        private void OnCurrentCheckInTimeCommandExecuted()
+        {
+            TodayCheckInTime = DateTime.Now;
+
+
+        }
+
+        public RelayCommand CurrentCheckInTimeCommand { get; set; }
+        public RelayCommand InputCheckInTimeCommand { get; set; }
+        private string _remainingTime;
+
+        public string RemainingTime
+        {
+            get { return _remainingTime; }
+            set
+            {
+
+                Set(ref _remainingTime, value);
+            }
+        }
+        public TimeSpan MaxHrs { get; set; } = new TimeSpan(9, 0, 0);
+        private DateTime? _todayCheckIntime;
+        public DateTime? TodayCheckInTime
+        {
+            get { return _todayCheckIntime; }
+            set
+            {
+                Set(ref _todayCheckIntime, value);
+            }
         }
 
         private string _currentTime;
@@ -21,14 +58,21 @@ namespace oClock.Shared.ViewModels
             get { return _currentTime; }
             set
             {
-                _currentTime = value;
-                OnPropertyChanged("CurrentTime");
+                Set(ref _currentTime, value);
             }
         }
-
         private void Timer_Tick(object sender, object e)
         {
             CurrentTime = DateTime.Now.ToString("hh:mm:ss");
+            if (TodayCheckInTime == null)
+            {
+                RemainingTime = "--:--:--";
+                return;
+            }
+            var time = DateTime.Now.Subtract(TodayCheckInTime.Value);
+            var rem = MaxHrs.Subtract(time);
+            RemainingTime = rem.ToString();
+
         }
     }
 }
