bf01319453046201bea4b60b1868801d65254834
Avnish Kumar <avnish@geninf.com>
2020-01-03 20:56:48 +0530

    Local storage fix in android

--- oClock/oClock.Shared/Helpers/LocalSettingsHelper.cs
+++ oClock/oClock.Shared/Helpers/LocalSettingsHelper.cs
@@ -9,16 +9,27 @@ namespace oClock.Shared.Helpers
 
         public static void MarkContainer<T>(SettingContainer container, string containerValue, T value)
         {
+#if !NETFX_CORE
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(value);
+			LocalSettings.Values[containerValue] = json;
+#else
             LocalSettings.Containers[container.ToString()].Values[containerValue] = value != null ? JsonConvert.SerializeObject(value) : null;
+#endif
+
         }
 
         public static T GetContainerValue<T>(SettingContainer container, string containerValue)
         {
+#if !NETFX_CORE
+           var json = (string)ApplicationData.Current.LocalSettings.Values[containerValue];
+			return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json);
+#else
             if (!(LocalSettings.Containers[container.ToString()].Values[containerValue] is string currentValue))
             {
                 return default;
             }
             return JsonConvert.DeserializeObject<T>(currentValue);
+#endif
         }
     }
 
