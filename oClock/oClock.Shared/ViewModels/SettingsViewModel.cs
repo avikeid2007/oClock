a0aa3e49c916185a1c7dc437ba9b6d9e934d1657
Avnish Kumar <avnish@geninf.com>
2020-01-10 22:33:18 +0530

    Setting Page Changes

--- oClock/oClock.Shared/ViewModels/SettingsViewModel.cs
+++ oClock/oClock.Shared/ViewModels/SettingsViewModel.cs
@@ -1,8 +1,26 @@
 ﻿using BasicMvvm;
+using BasicMvvm.Commands;
+using Windows.UI.Xaml.Controls;
 
 namespace oClock.Shared.ViewModels
 {
     public class SettingsViewModel : BindableBase
     {
+        public SettingsViewModel()
+        {
+            BackCommand = new DelegateCommand<object>(OnBackCommandExecute);
+        }
+
+        public DelegateCommand<object> BackCommand { get; }
+
+        private void OnBackCommandExecute(object obj)
+        {
+            var page = obj as Page;
+            if (page != null)
+            {
+                if (page.Frame.CanGoBack)
+                    page.Frame.GoBack();
+            }
+        }
     }
 }
